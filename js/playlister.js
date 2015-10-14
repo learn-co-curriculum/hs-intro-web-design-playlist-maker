8426e98bef1a99548be3f0902e34cf9d61ef63e9
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-14 14:19:22 -0400

    solutions

--- js/playlister.js
+++ js/playlister.js
@@ -14,16 +14,123 @@ choice = prompt("Would you like to add or delete a song?");
 if (choice == "add"){
   var song = prompt("What song would you like to add?");
   playlist.push(song);
-  alert(playlist)
+  alert(playlist);
 }
 else if (choice == "delete"){
     var song = prompt("What song would you like to add?");
     var index = playlist.indexOf(song)
     playlist.splice(index, 1);
-    alert(playlist)
+    alert(playlist);
 }
 else {
   alert("goodbye.")
 }
 
 
+//BONUS 1:
+// var playlist = [];
+
+// var choice = prompt("Your playlist is empty, would you like to add a song (yes, no)?");
+
+// if (choice == "yes"){
+//   var song = prompt("What song would you like to add?");
+//   playlist.push(song);
+// }
+
+// choice = prompt("Would you like to add or delete a song?");
+
+// if (choice == "add"){
+//   var song = prompt("What song would you like to add?");
+//   playlist.push(song);
+//   alert(playlist);
+// }
+// else if (choice == "delete"){
+//     var song = prompt("What song would you like to delete?");
+//     var index = playlist.indexOf(song)
+//     if (index == -1){
+//       alert("That song is not on your playlist")
+//     }
+//     else { 
+//       playlist.splice(index, 1);
+//     }
+//     alert(playlist);
+// }
+// else {
+//   alert("goodbye.")
+// }
+
+
+//******BONUS 2********
+// var playlist = [];
+
+// var choice = prompt("Your playlist is empty, would you like to add a song (yes, no)?");
+
+// if (choice == "yes"){
+//   var song = prompt("What song would you like to add?");
+//   playlist.push(song);
+// }
+
+// var next = "yes"
+// while (next == "yes"){
+//   choice = prompt("Would you like to add or delete a song?");
+
+//   if (choice == "add"){
+//     var song = prompt("What song would you like to add?");
+//     playlist.push(song);
+//     alert("Your playlist contains:" + playlist);
+//     next = prompt("Would you like to continue? (yes, no)");
+
+//   }
+//   else if (choice == "delete"){
+//       var song = prompt("What song would you like to add?");
+//       var index = playlist.indexOf(song)
+//       if (index == -1){
+//         alert("That song is not on your playlist")
+//       }
+//       playlist.splice(index, 1);
+//       alert("Your playlist contains:" + playlist);
+//       next = prompt("Would you like to continue? (yes, no)");
+//   }
+// }
+
+//**** DOUBLE BONUS***
+
+// var playlist = [];
+
+// function addSong(){
+//   var song = prompt("What song would you like to add?");
+//     playlist.push(song);
+//     alert("Your playlist contains:" + playlist);
+// }
+
+// function deleteSong(){
+//   var song = prompt("What song would you like to delete?");
+//       var index = playlist.indexOf(song)
+//       if (index == -1){
+//         alert("That song is not on your playlist")
+//       }
+//       playlist.splice(index, 1);
+//       alert("Your playlist contains:" + playlist);
+// }
+
+// var choice = prompt("Your playlist is empty, would you like to add a song (yes, no)?");
+
+// if (choice == "yes"){
+//   var song = prompt("What song would you like to add?");
+//   playlist.push(song);
+// }
+
+// var next = "yes"
+// while (next == "yes"){
+//   choice = prompt("Would you like to add or delete a song?");
+
+//   if (choice == "add"){
+//     addSong();
+//     next = prompt("Would you like to continue? (yes, no)");
+
+//   }
+//   else if (choice == "delete"){
+//       deleteSong();
+//       next = prompt("Would you like to continue? (yes, no)");
+//   }
+// }
